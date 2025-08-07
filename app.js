3abeb5d0a026ba652d7cd0aaf1772361416239d9
Its-Adarshkumar <adarshsinghrajput414@gmail.com>
2025-08-07 22:51:39 +0530

    project setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const app=express();
+const mongoose=require('mongoose');
+const path=require("path");
+const ejsMate=require("ejs-mate");
+
+
+app.get("/",(req,res)=>{
+    res.send("Hi , I am root");
+})
+
+
+app.set('view engine',"ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(8080,()=>{
+    console.log('server is listening to port 8080');
+})
